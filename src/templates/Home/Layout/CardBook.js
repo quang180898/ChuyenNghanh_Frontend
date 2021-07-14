2dab4f7727f35b67cf414fde4700074c780620c5
QuangNguyen <quangnd@minerva.vn>
2021-07-15 03:12:11 +0700

    update css image

--- src/templates/Home/Layout/CardBook.js
+++ src/templates/Home/Layout/CardBook.js
@@ -32,21 +32,21 @@ const CardBook = ({ title, image, totalSize, id, totalBorrow, product }) => {
         let cartCopy = [...state];
 
         const oldproduct = localStorage.getItem('cart') ? localStorage.getItem('cart') : "[]";
-        const arrayproduct =  JSON.parse(oldproduct);  
+        const arrayproduct = JSON.parse(oldproduct);
         let products = product
 
         let existingItem = cartCopy.find(cartItem => cartItem.id == id);
 
         if (existingItem) {
             showNotification.error({ message: 'Sách đã có trong giỏ', title: 'waring' })
-        } else { 
+        } else {
             showNotification.success({ message: 'Sách đã được thêm vào giỏ hàng', title: 'success' })
             arrayproduct.push(products);
         }
-    
+
         localStorage.setItem('cart', JSON.stringify(arrayproduct));
-        dispatch(cartAction.addToCart(arrayproduct))                   
-    } 
+        dispatch(cartAction.addToCart(arrayproduct))
+    }
 
     const onMoveDetail = (id) => {
         history.push(PAGES_URL.product.url + "/" + id);
@@ -59,15 +59,15 @@ const CardBook = ({ title, image, totalSize, id, totalBorrow, product }) => {
                 cover
             >
                 <div className="item">
-                    <figure className="image">
+                    <div className="image">
                         <img src={`data:image/jpeg;base64,${image}`} />
                         <a className="link" onClick={() => onMoveDetail(id)}>Chi Tiết</a>
-                    </figure>
-                </div>
-                <div className="text-center">{title}</div>
-                <div className="support-book">
-                    <ButtonStyle className="btn-white" label="Mượn sách" onClick={() => addProductToCart(id)}></ButtonStyle>
-                    {totalSize ? <span>Số lượng: {totalSize}</span> : <span>Lượt mượn: {totalBorrow}</span>}
+                    </div>
+                    <div className="text-center">{title}</div>
+                    <div className="support-book">
+                        <ButtonStyle className="btn-white" label="Mượn sách" onClick={() => addProductToCart(id)}></ButtonStyle>
+                        {totalSize ? <span>Số lượng: {totalSize}</span> : <span>Lượt mượn: {totalBorrow}</span>}
+                    </div>
                 </div>
             </Card>
         </div>
